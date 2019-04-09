7fa3f18d8c07f01f923e40055dd04a9e753efc26
AyoubXwire <benayoubid@gmail.com>
2019-04-09 21:37:51 +0100

    execute test query

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use('/administration', require('./routes/administration'))
 // Listener
 app.listen(process.env.PORT || 3000, () => console.log('server running..'))
 
-/*
+
 let command = `
     CREATE TABLE IF NOT EXISTS users (
         id INTEGER NOT NULL auto_increment,
@@ -89,58 +89,7 @@ let command = `
         FOREIGN KEY (id_filiere) REFERENCES filieres(id)
     );
 
-    -- insert in secteur
-    INSERT INTO secteurs(code, nom, description) VALUES("NTIC", "Technologies de l'information", "Le Maroc est conscient du fait que l’usage des technologies de l’information est un facteur essentiel pour l’émergence de la société du savoir et peut activement contribuer au développement humain, à l’amélioration de la cohésion sociale et à la croissance de l’économie nationale.");
-    INSERT INTO secteurs(code, nom, description) VALUES("AGC", "Administration Gestion et Commerce", "Les nouvelles tendances de l’économie mondiale montrent que les métiers du Tertiaire prennent une place de plus en plus importante dans le système de production en général en vue d’une plus grande productivité.");
-    INSERT INTO secteurs(code, nom, description) VALUES("TH", "Textile et Habillement", "L’industrie du Textile et de l’Habillement occupe une place stratégique dans l’industrie nationale de transformation aussi bien sur le plan des emplois et des exportations que sur le plan de l’équilibre socio-économique du pays.");
-
-    -- insert in filiere
-    INSERT INTO filieres(code, nom, description, id_secteur) VALUES('TDI', "Techniques de développement informatiques (TS)", "description du filiere", 1);
-    INSERT INTO filieres(code, nom, description, id_secteur) VALUES('TRI', "Techniques des réseaux informatiques (TS)", "description du filiere", 1);
-    INSERT INTO filieres(code, nom, description, id_secteur) VALUES('TMSIR', "Technicien en Maintenance et Systèmes Informatiques et Réseaux (T)", "description du filiere", 1);
-    INSERT INTO filieres(code, nom, description, id_secteur) VALUES('TSGE', "Technicien Spécialisé en Gestion des Entreprises (TS)", "description du filiere", 2);
-    INSERT INTO filieres(code, nom, description, id_secteur) VALUES('TCE', "Technicien Comptable d’Entreprises (T)", "description du filiere", 2);
-    INSERT INTO filieres(code, nom, description, id_secteur) VALUES('TSB', "Technicien en Secrétariat Bureautique (T)", "description du filiere", 2);
-    INSERT INTO filieres(code, nom, description, id_secteur) VALUES('THP', "Techniques Habillement Production (T)", "description du filiere", 3);
-    INSERT INTO filieres(code, nom, description, id_secteur) VALUES('THI', "Technique Habillement Industrialisation (T)", "description du filiere", 3);
-    INSERT INTO filieres(code, nom, description, id_secteur) VALUES('TMI', "Technique Modélisme Industriel (T)", "description du filiere", 3);
-    INSERT INTO filieres(code, nom, description, id_secteur) VALUES('TMMC', "Techniques Maintenance de Machines à Coudre (T)", "description du filiere", 3);
-
-    -- insert in module
-    INSERT INTO modules(nom, id_filiere) VALUES('arabe', 1);
-    INSERT INTO modules(nom, id_filiere) VALUES('arabe', 2);
-    INSERT INTO modules(nom, id_filiere) VALUES('arabe', 3);
-    INSERT INTO modules(nom, id_filiere) VALUES('francais', 1);
-    INSERT INTO modules(nom, id_filiere) VALUES('francais', 2);
-    INSERT INTO modules(nom, id_filiere) VALUES('francais', 3);
-    INSERT INTO modules(nom, id_filiere) VALUES('anglais', 1);
-    INSERT INTO modules(nom, id_filiere) VALUES('anglais', 2);
-    INSERT INTO modules(nom, id_filiere) VALUES('anglais', 3);
-    INSERT INTO modules(nom, id_filiere) VALUES('francais', 1);
-    INSERT INTO modules(nom, id_filiere) VALUES('francais', 2);
-    INSERT INTO modules(nom, id_filiere) VALUES('francais', 3);
-    INSERT INTO modules(nom, id_filiere) VALUES('anglais', 1);
-    INSERT INTO modules(nom, id_filiere) VALUES('anglais', 2);
-    INSERT INTO modules(nom, id_filiere) VALUES('anglais', 3);
-    INSERT INTO modules(nom, id_filiere) VALUES('c#', 1);
-    INSERT INTO modules(nom, id_filiere) VALUES('algorithme', 1);
-    INSERT INTO modules(nom, id_filiere) VALUES('database', 1);
-    INSERT INTO modules(nom, id_filiere) VALUES('backend', 1);
-    INSERT INTO modules(nom, id_filiere) VALUES('frontend', 1);
-    INSERT INTO modules(nom, id_filiere) VALUES('maintenance', 3);
-    INSERT INTO modules(nom, id_filiere) VALUES('VLSM', 3);
-    INSERT INTO modules(nom, id_filiere) VALUES('routage', 3);
-    INSERT INTO modules(nom, id_filiere) VALUES('commutation', 3);
-    INSERT INTO modules(nom, id_filiere) VALUES('securite', 2);
-    INSERT INTO modules(nom, id_filiere) VALUES('DNS', 2);
-    INSERT INTO modules(nom, id_filiere) VALUES('AD', 2);
-    INSERT INTO modules(nom, id_filiere) VALUES('DHCP', 2);
-    INSERT INTO modules(nom, id_filiere) VALUES('entreprenariat', 1);
-    INSERT INTO modules(nom, id_filiere) VALUES('entreprenariat', 2);
-    INSERT INTO modules(nom, id_filiere) VALUES('entreprenariat', 3);
-    INSERT INTO modules(nom, id_filiere) VALUES('emploi', 1);
-    INSERT INTO modules(nom, id_filiere) VALUES('emploi', 2);
-    INSERT INTO modules(nom, id_filiere) VALUES('emploi', 3);
+    update users set power = 3 where username = admin;
 `
     
 let params = []
@@ -156,4 +105,3 @@ pool.getConnection((error, connection) => {
         connection.release()
     })
 })
-*/
