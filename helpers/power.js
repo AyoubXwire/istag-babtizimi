5b023fa8639cea5eb5f255c17598cccad882e18b
AyoubXwire <benayoubid@gmail.com>
2019-04-09 15:59:52 +0100

    Fix isOwner

--- helpers/power.js
+++ helpers/power.js
@@ -10,15 +10,15 @@ module.exports = {
     isOwer: (req, res, next) => {
         let command = `SELECT user_id FROM posts WHERE id = ?`
         let params = [req.params.id]
-
+        
         pool.getConnection((error, connection) => {
             if(error) throw error
 
             connection.query(command, params, (error, rows) => {
                 if(error) throw error
-                let postOwner = rows[0].id
+                let postOwner = rows[0].user_id
                 connection.release()
-
+                
                 if(req.user.id === postOwner) {
                     return next()
                 }
