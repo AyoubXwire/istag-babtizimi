8d0e22e37110b1706728bfafc8a02ac461f792ae
AyoubXwire <benayoubid@gmail.com>
2019-04-09 21:29:47 +0100

    Fix env variables

--- config/session.js
+++ config/session.js
@@ -3,21 +3,21 @@ const mysqlStore = require('express-mysql-session')(session)
 
 let sessionStore
 
-sessionStore = new mysqlStore({
-    host     : 'sql7.freesqldatabase.com',
-    user     : 'sql7286630',
-    password : 'fjbuICKb4e',
-    database : 'sql7286630'
-})
-// if(process.env === 'production') {
-// } else {
-//     sessionStore = new mysqlStore({
-//         host     : 'localhost',
-//         user     : 'root',
-//         password : 'xwire',
-//         database : 'babtizimi'
-//     })
-// }
+if(process.env === 'production') {
+    sessionStore = new mysqlStore({
+        host     : 'sql7.freesqldatabase.com',
+        user     : 'sql7286630',
+        password : 'fjbuICKb4e',
+        database : 'sql7286630'
+    })
+} else {
+    sessionStore = new mysqlStore({
+        host     : 'localhost',
+        user     : 'root',
+        password : 'xwire',
+        database : 'babtizimi'
+    })
+}
 
 module.exports = session({
     secret: 'cosmicsecret',
