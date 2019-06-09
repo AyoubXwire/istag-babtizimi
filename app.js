96a23748525425c5f4ef73416a1aa362a458b8fd
AyoubXwire <benayoubid@gmail.com>
2019-06-09 05:22:39 +0100

    Optimize speed
    Compress images
    Remove preloader

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const server = http.createServer(app)
 const io     = socketio(server)
 
 // Config & middleware
-app.use(compression())
+app.use(compression({ level: 9 }))
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
--- public/js/active.js
+++ public/js/active.js
@@ -3,12 +3,12 @@
 
     var browserWindow = $(window);
 
-    // :: 1.0 Preloader Active Code
-    browserWindow.on('load', function () {
-        $('#preloader').fadeOut('slow', function () {
-            $(this).remove();
-        });
-    });
+    // // :: 1.0 Preloader Active Code
+    // browserWindow.on('load', function () {
+    //     $('#preloader').fadeOut('slow', function () {
+    //         $(this).remove();
+    //     });
+    // });
 
     // :: 2.0 Nav Active Code
     if ($.fn.classyNav) {
